d70dd9404200243bd7d7b68d33a0c27ed73c7b40
bismark <bismarkakwah86@gmail.com>
2025-03-31 19:47:57 +0000

    cookies issues fixed

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,12 +5,19 @@ const generateToken = async(userId, res)=>{
     expiresIn:"7d"
  });
 
- res.cookie("jwt",token, {
-    maxAge:7*24*60*60*1000,
-    httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV === "development",
- });
+//  res.cookie("jwt",token, {
+//     maxAge:7*24*60*60*1000,
+//     httpOnly:true,
+//     sameSite:"strict",
+//     secure:process.env.NODE_ENV === "development",
+//  });
+
+res.cookie("jwt",token, {
+   maxAge:7*24*60*60*1000,
+   httpOnly:true,
+   sameSite:"none",
+   secure:process.env.NODE_ENV === "production"
+});
 
  return token;
 }
